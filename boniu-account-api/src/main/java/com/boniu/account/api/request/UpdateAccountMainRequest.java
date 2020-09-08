4b28c23707271d3f825bdf21af0cd92b11b67289
HanXin <hanxin@rhinox.cn>
2020-09-08 18:27:49 +0800

    个人中心

---
+++ boniu-account-api/src/main/java/com/boniu/account/api/request/UpdateAccountMainRequest.java
@@ -0,0 +1,64 @@
+package com.boniu.account.api.request;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+/**
+ * @Description 更新主账户信息入参
+ * @Author hanxin
+ * @Date 2020/9/8
+ */
+
+@ApiModel("更新主账户信息入参")
+public class UpdateAccountMainRequest {
+    @ApiModelProperty(value = "主账户", example = "100238947897468", required = true)
+    private String accountMainId;
+    @ApiModelProperty(value = "个人中心昵称", example = "昵称", required = false)
+    private String nickname;
+    @ApiModelProperty(value = "个人中心头像链接地址", example = "http://boniuapp.oss-cn-hangzhou.aliyuncs.com/common/head/1599546812784.png", required = false)
+    private String headImg;
+    @ApiModelProperty(value = "累计获得总积分", example = "10000", required = false)
+    private Integer totalScore;
+    @ApiModelProperty(value = "当前可用积分", example = "2000", required = false)
+    private Integer remainScore;
+
+    public String getAccountMainId() {
+        return accountMainId;
+    }
+
+    public void setAccountMainId(String accountMainId) {
+        this.accountMainId = accountMainId;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getHeadImg() {
+        return headImg;
+    }
+
+    public void setHeadImg(String headImg) {
+        this.headImg = headImg;
+    }
+
+    public Integer getTotalScore() {
+        return totalScore;
+    }
+
+    public void setTotalScore(Integer totalScore) {
+        this.totalScore = totalScore;
+    }
+
+    public Integer getRemainScore() {
+        return remainScore;
+    }
+
+    public void setRemainScore(Integer remainScore) {
+        this.remainScore = remainScore;
+    }
+}
